d7f531206d0bec5029c7ca509add5bb63c2de473
HansikaPerera1117 <hansikaperera59@gmail.com>
2022-07-12 10:04:24 +0530

    create appinitializer packages and studentForm.fxml and studentformController classes

---
+++ src/Appinitialize.java
@@ -0,0 +1,21 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Appinitialize extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("view/studentForm.fxml"))));
+        primaryStage.setTitle("Student Registration System");
+        primaryStage.centerOnScreen();
+        primaryStage.show();
+    }
+}
