4c7b16a147ca07018df3fa29e1ed6c5e220734d3
HansikaPerera1117 <hansikaperera59@gmail.com>
2022-07-12 10:16:16 +0530

    create tm package in view packege and create studentTm class and add properties

---
+++ src/view/tm/StudentTM.java
@@ -0,0 +1,13 @@
+package view.tm;
+
+import javafx.scene.control.Button;
+
+public class StudentTM {
+    private String student_id;
+    private String student_name;
+    private String email;
+    private String contact;
+    private String address;
+    private String nic;
+    Button btn;
+}
