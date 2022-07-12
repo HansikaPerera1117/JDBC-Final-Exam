8d4d42aa4490471c50dbac08381716e68809d7d7
HansikaPerera1117 <hansikaperera59@gmail.com>
2022-07-12 12:16:04 +0530

    implement codes in  validationUtil class

--- src/util/ValidationUtil.java
+++ src/util/ValidationUtil.java
@@ -1,4 +1,33 @@
 package util;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.scene.control.TextField;
+
+import java.util.LinkedHashMap;
+import java.util.regex.Pattern;
+
 public class ValidationUtil {
+    public static Object validate(LinkedHashMap<TextField, Pattern> map, JFXButton btn) {
+        for (TextField key : map.keySet()) {
+            Pattern pattern = map.get(key);
+            if (!pattern.matcher(key.getText()).matches()){
+                addError(key,btn);
+                return key;
+            }
+            removeError(key,btn);
+        }
+        return true;
+    }
+
+    private static void removeError(TextField txtField,JFXButton btn) {
+        txtField.setStyle("-fx-text-fill:#116530");
+        btn.setDisable(false);
+    }
+
+    private static void addError(TextField txtField,JFXButton btn) {
+        if (txtField.getText().length() > 0) {
+            txtField.setStyle("-fx-text-fill: red");
+        }
+        btn.setDisable(true);
+    }
 }
